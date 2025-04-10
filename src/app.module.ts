b0adbf59502b839e4af00bba936028509f6cefcd
Leroyalle <nikolajmelonov@yandex.ru>
2025-04-10 18:07:06 +0300

    feat(listing): add listing module, typeorm and graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,37 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { ListingModule } from './listing/listing.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       // sortSchema: true,
+      playground: true,
+      // installSubscriptionHandlers: true,
+      buildSchemaOptions: {
+        dateScalarMode: 'timestamp',
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
     }),
     ListingModule,
   ],
--- src/listing/entities/listing.entity.ts
+++ src/listing/entities/listing.entity.ts
@@ -1,16 +1,26 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Listing {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID, { description: 'Название объявления' })
+  id: string;
+
+  @Column()
   @Field({ description: 'Название объявления' })
   name: string;
 
+  @Column()
   @Field(() => Int, { description: 'Цена объявления' })
   price: number;
 
+  @Column()
   @Field({ description: 'Описание объявления' })
   description: string;
 
+  @Column('text', { array: true })
   @Field(() => [String], { description: 'Изображения объявления' })
-  image: string[];
+  images: string[];
 }
